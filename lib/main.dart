73d20b71d30d6e185b9cd3b27e78e7a8d373d6bc
Do Nguyen <daniel@zincer.com>
2018-05-25 08:21:01 +0700

    Update Android Build Config

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import './screens/Home.dart';
 
 void main() => runApp(new App());
 
-String selectedUrl = "https://flutter.io";
-
 class App extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
